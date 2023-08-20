767e78937999b9c63a5950dab1ddb061055d8ce6
dahesey <dahesey@gmail.com>
2023-08-20 10:06:59 +0000

    A functiont to print to stdout

---
+++ output_format.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+  * _printf - A function that produces output according to a format
+  * Authors - Collins & Cynthia
+  * @format: format to be printed
+  * @coll: number of characters printed
+  * Return: Number of characters printed
+  */
+int _printf(const char *format, ...)
+{
+	int coll = 0, a = 0;
+	va_list cyn;
+
+	va_start(cyn, format);
+
+	if (format != NULL)
+	{
+		while (format[a])
+		{
+			if (format[a] == '%')
+			{
+				format++;
+				if (format[a] == 'c')
+				{
+					int m = va_arg(cyn, int);
+
+					putchar(m);
+					coll++;
+				}
+				if (format[a] == 's')
+				{
+					int *s = va_arg(cyn, int *);
+
+					while (*s != '\0')
+					{
+						putchar(*s);
+						s++;
+					}
+				}
+				if (format[a] == '%')
+					putchar('%');
+					coll++;
+			}
+			else
+				putchar(format[a]);
+				coll++;
+		format++;
+		}
+	}
+	va_end(cyn);
+	return (coll);
+}
