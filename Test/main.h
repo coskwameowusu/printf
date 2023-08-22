8a8d2954857267c12530c9721a44e4d20ed3b6c8
Collins Owusu <coskwameowusu@gmail.com>
2023-08-22 05:22:40 -0700

    collins

---
+++ Test/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int cc_putchar(char c);
+int printInteger(int num);
+
+#endif /* MAIN_H */
