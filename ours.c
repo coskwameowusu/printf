25caf0a148bcbd8bc6428706060f60eb7ea50569
Collins Owusu <coskwameowusu@gmail.com>
2023-08-22 17:04:46 -0700

    new try code

---
+++ ours.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: The format string.
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int coll = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int m = va_arg(args, int);
+
+				putchar(m);
+				coll++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				while (*s)
+				{
+					putchar(*s);
+					s++;
+					coll++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				coll++;
+			}
+			format++;
+		}
+		else
+		{
+			putchar(*format);
+			format++;
+			coll++;
+		}
+	}
+
+	va_end(args);
+	return coll;
+}
+
